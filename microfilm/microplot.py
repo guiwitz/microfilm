0a3a52bd6613f17ce475426fbbf532f230759681
guiwitz <guiwitz@gmail.com>
2021-04-25 01:28:28 +0200

    new Microimage class with methods to add scalebar and labels

--- microfilm/microplot.py
+++ microfilm/microplot.py
@@ -1,4 +1,5 @@
 import warnings
+import inspect
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,7 +8,7 @@ from matplotlib.colors import ListedColormap
 from matplotlib.patches import Rectangle
 
 
-def colormap_from_name(image, cmap_name, flip_map=False, rescale_type='min_max', limits=None):
+def colormap_from_name(image, cmap_name, flip_map=False, rescale_type='min_max', limits=None, num_colors=256):
     """
     Return 2D image as 3D RGB stack colored with a given colormap.
     
@@ -26,22 +27,23 @@ def colormap_from_name(image, cmap_name, flip_map=False, rescale_type='min_max',
         'limits': between limits given by parameter limits
     limits: list
         [min, max] limits to use for rescaling
+    num_colors: int
+        number of steps in color scale
     """
     
-    steps = 256
     image = rescale_image(image, rescale_type=rescale_type, limits=limits)
     
     try:
-        cmap = plt.get_cmap(cmap_name, steps)
+        cmap = plt.get_cmap(cmap_name, num_colors)
     except ValueError:
         if cmap_name == 'pure_red':
-            cmap = ListedColormap(np.c_[np.linspace(0,1,steps), np.zeros(steps), np.zeros(steps)])
+            cmap = ListedColormap(np.c_[np.linspace(0,1,num_colors), np.zeros(num_colors), np.zeros(num_colors)])
         if cmap_name == 'pure_green':
-            cmap = ListedColormap(np.c_[np.zeros(steps), np.linspace(0,1,steps), np.zeros(steps)])
+            cmap = ListedColormap(np.c_[np.zeros(num_colors), np.linspace(0,1,num_colors), np.zeros(num_colors)])
         if cmap_name == 'pure_blue':
-            cmap = ListedColormap(np.c_[np.zeros(steps), np.zeros(steps), np.linspace(0,1,steps)])
+            cmap = ListedColormap(np.c_[np.zeros(num_colors), np.zeros(num_colors), np.linspace(0,1,num_colors)])
         if cmap_name == 'segmentation':
-            cmap = random_cmap()
+            cmap = random_cmap(num_colors=num_colors)
     if flip_map:
             cmap = cmap.reversed()
             
@@ -49,7 +51,8 @@ def colormap_from_name(image, cmap_name, flip_map=False, rescale_type='min_max',
     
     return image_colored
     
-def colormap_from_colorhex(image, cmap_hex='#ff6600', flip_map=False, rescale_type='min_max', limits=None):
+def colormap_from_colorhex(image, cmap_hex='#ff6600', flip_map=False, rescale_type='min_max',
+                           limits=None, num_colors=256):
     """
     Return 2D image as 3D RGB stack colored with a hex color.
     
@@ -68,14 +71,15 @@ def colormap_from_colorhex(image, cmap_hex='#ff6600', flip_map=False, rescale_ty
         'limits': between limits given by parameter limits
     limits: list
         [min, max] limits to use for rescaling
+    num_colors: int
+        number of steps in color scale
     """
     
     image = rescale_image(image, rescale_type=rescale_type, limits=limits)
-    steps = 256
-    chosen_col = np.array(list(int(cmap_hex[i:i+2], 16) for i in (1, 3, 5)))/255
-    new_col_scale = np.c_[np.linspace(0,chosen_col[0],steps),
-                          np.linspace(0,chosen_col[1],steps),
-                          np.linspace(0,chosen_col[2],steps)]
+    chosen_col = np.array(list(int(cmap_hex[i:i+2], 16) for i in (1, 3, 5)))/(num_colors-1)
+    new_col_scale = np.c_[np.linspace(0,chosen_col[0],num_colors),
+                          np.linspace(0,chosen_col[1],num_colors),
+                          np.linspace(0,chosen_col[2],num_colors)]
     cmap = ListedColormap(new_col_scale)
     if flip_map:
         cmap = cmap.reversed()
@@ -83,10 +87,10 @@ def colormap_from_colorhex(image, cmap_hex='#ff6600', flip_map=False, rescale_ty
     
     return image_colored
 
-def random_cmap(alpha=0.5):
+def random_cmap(alpha=0.5, num_colors=256):
     """Create random colormap for segmentation"""
     
-    colmat = np.random.rand(256,4)
+    colmat = np.random.rand(num_colors,4)
     colmat[:,-1] = alpha
     colmat[0,-1] = 0
     cmap = matplotlib.colors.ListedColormap(colmat)
@@ -144,7 +148,8 @@ def combine_image(images):
     
     return im_combined
 
-def multichannel_to_rgb(images, cmaps=None, flip_map=False, rescale_type='min_max', limits=None):
+def multichannel_to_rgb(images, cmaps=None, flip_map=False, rescale_type='min_max',
+                        limits=None, num_colors=256):
     """
     Convert a list of images to a single RGB image. Options can be passed
     as lists, one per channel, or as single element in which case the same value is used
@@ -165,6 +170,8 @@ def multichannel_to_rgb(images, cmaps=None, flip_map=False, rescale_type='min_ma
         'limits': between limits given by parameter limits
     limits: list or list of lists
         [min, max] limits to use for rescaling
+    num_colors: int
+        number of steps in color scale
         
     Returns
     -------
@@ -194,7 +201,8 @@ def multichannel_to_rgb(images, cmaps=None, flip_map=False, rescale_type='min_ma
             im, cmap_name=cmaps[ind],
             flip_map=flip_map[ind],
             rescale_type=rescale_type[ind],
-            limits=limits[ind]) for ind, im in enumerate(images)
+            limits=limits[ind],
+            num_colors=num_colors) for ind, im in enumerate(images)
         ])
     
     return converted
@@ -219,7 +227,7 @@ def check_input(images):
     return images
             
     
-def microshow(images, cmaps=None, flip_map=False, rescale_type='min_max', limits=None,
+def microshow(images, cmaps=None, flip_map=False, rescale_type='min_max', limits=None, num_colors=256,
               scalebar=False, height_pixels=3, unit_per_pix=None, scalebar_units=None, unit=None,
               scale_ypos=0.05, scale_color='white', scale_font_size=12,
               scale_text_centered=False, ax=None
@@ -242,6 +250,8 @@ def microshow(images, cmaps=None, flip_map=False, rescale_type='min_max', limits
         'limits': between limits given by parameter limits
     limits: list or list of lists
         [min, max] limits to use for rescaling
+    num_colors: int
+        number of steps in color scale
     scalebar: bool
         add scale bar or not
     height_pixels: int
@@ -264,12 +274,13 @@ def microshow(images, cmaps=None, flip_map=False, rescale_type='min_max', limits
             
     Returns
     -------
-    fig, ax
+    Microimage object
     
     """
     
     images = check_input(images)
-    converted = multichannel_to_rgb(images, cmaps=cmaps, flip_map=flip_map, rescale_type=rescale_type, limits=limits)
+    converted = multichannel_to_rgb(images, cmaps=cmaps, flip_map=flip_map,
+                                    rescale_type=rescale_type, limits=limits, num_colors=num_colors)
     
     height = images[0].shape[0]
     width = images[1].shape[0]
@@ -289,71 +300,52 @@ def microshow(images, cmaps=None, flip_map=False, rescale_type='min_max', limits
     ax.imshow(converted, interpolation='nearest')
     ax.set_axis_off()
     
+    microim = Microimage(ax)
+    
     if scalebar:
         
-        if (unit is None) or (scalebar_units is None) or (unit_per_pix is None):
-            raise Exception(f"You need to provide a unit (unit), scale (unit_per_pix) and size of your scale bar (scalebar_units)")
-            
-        height_pixels /= images[0].shape[0]
-        
-        pixelsize = scalebar_units / unit_per_pix
         image_width = images[0].shape[1]
+        pixelsize = scalebar_units / unit_per_pix
         scale_width = pixelsize / image_width
+        microim.add_scalebar(unit, scalebar_units, unit_per_pix, height_pixels=height_pixels,
+                             scale_ypos=scale_ypos, scale_color=scale_color,
+                             scale_font_size=scale_font_size, scale_text_centered=scale_text_centered)
         
-        if unit =='um':
-            scale_text = f'{scalebar_units} $\mu$m'
-        else:
-            scale_text = f'{scalebar_units} {unit}'
-            
-
-        scale_bar = Rectangle((1-scale_width-0.05, scale_ypos), width=scale_width, height=height_pixels,
-                          transform=ax.transAxes, facecolor=scale_color)
-        if scale_font_size is not None:
-            scale_text = ax.text(x=1-scale_width-0.05, y=scale_ypos+height_pixels+0.02, s=scale_text,
-                         transform=ax.transAxes, fontdict={'color':scale_color, 'size':scale_font_size})
-        
-            if scale_text_centered:
-                # trick https://stackoverflow.com/questions/5320205/matplotlib-text-dimensions
-                r = fig.canvas.get_renderer()
-                text_width = scale_text.get_window_extent(renderer=r).width / images[0].shape[1]
-                bar_middle = 1-0.5*scale_width-0.05
-                text_start = bar_middle - 0.5*text_width
-                scale_text.set_x(text_start)
-        
-        ax.add_patch(scale_bar)
-        
-    microim = Microimage(ax.figure, ax)
-        
-    return microim#ax.figure, ax
-
-def add_scalebar(ax, unit, scalebar_units, unit_per_pix, height_pixels=3, scale_ypos=0.05,
-                scale_color='white'):
-          
-    if (unit is None) or (scalebar_units is None) or (unit_per_pix is None):
-        raise Exception(f"You need to provide a unit (unit), scale (unit_per_pix) and size of your scale bar (scalebar_units)")
-
-    height_pixels /= ax.get_images()[0].get_array().shape[0]
-
-    pixelsize = scalebar_units / unit_per_pix
-    image_width = ax.get_images()[0].get_array().shape[1]
-    scale_width = pixelsize / image_width
-    
-    scale_bar = Rectangle((1-scale_width-0.05, scale_ypos), width=scale_width, height=height_pixels,
-                          transform=ax.transAxes, facecolor=scale_color)
-
-    ax.add_patch(scale_bar)
-    return ax
+    microim = Microimage(ax)  
+    return microim
 
 
 class Microimage:
-    def __init__(self, fig, ax):
+    def __init__(self, ax):
         
-        self.fig, self.ax = (fig, ax)
-        
+        self.ax = ax        
     
     def add_scalebar(self, unit, scalebar_units, unit_per_pix, height_pixels=3, scale_ypos=0.05,
-        scale_color='white'):
+        scale_color='white', scale_font_size=12, scale_text_centered=False):
+        """
+        Add scalebar to an image.
 
+        Parameters
+        ----------
+        unit: str
+            name of the scale unit
+        scalebar_units: float
+            size of scale bar in true units
+        unit_per_pix: float
+            pixel scaling (e.g. 25um per pixel)
+        height_pixels: int
+            height of scale bar
+        scale_y_pos: float
+            y position of scale bar (0-1)
+        scale_color: str
+            color of scale bar
+        scale_font_size: int
+            size of text, set to None for no text
+        scale_text_centered: bool
+            center text above scale bar
+            
+        """
+        
         if (unit is None) or (scalebar_units is None) or (unit_per_pix is None):
             raise Exception(f"You need to provide a unit (unit), scale (unit_per_pix) and size of your scale bar (scalebar_units)")
 
@@ -363,8 +355,94 @@ class Microimage:
         image_width = self.ax.get_images()[0].get_array().shape[1]
         scale_width = pixelsize / image_width
 
-        scale_bar = Rectangle((1-scale_width-0.05, scale_ypos), width=scale_width, height=height_pixels,
+        if unit =='um':
+            scale_text = f'{scalebar_units} $\mu$m'
+        else:
+            scale_text = f'{scalebar_units} {unit}'
+        
+        bar_pad = 0.05
+        scale_bar = Rectangle((1-scale_width-bar_pad, scale_ypos), width=scale_width, height=height_pixels,
                               transform=self.ax.transAxes, facecolor=scale_color)
+        
+        if scale_font_size is not None:
+            
+            
+            scale_text = self.ax.text(x=0, y=scale_ypos+height_pixels+0.02, s=scale_text,
+                         transform=self.ax.transAxes, fontdict={'color':scale_color, 'size':scale_font_size})
+            text_start = 1-scale_width-bar_pad
+            
+            # trick https://stackoverflow.com/questions/5320205/matplotlib-text-dimensions
+            r = self.ax.figure.canvas.get_renderer()
+            ax_width = self.ax.get_tightbbox(r).width
+            text_width = scale_text.get_window_extent(renderer=r).width / ax_width
+            
+            if scale_text_centered:
+                bar_middle = 1-0.5*scale_width-bar_pad
+                text_start = bar_middle - 0.5*text_width
+            
+            # check if scale text outside image
+            if text_start + text_width > 0.98:
+                shift = text_start + text_width - 0.98
+                scale_bar = Rectangle((1-scale_width-bar_pad-shift, scale_ypos), width=scale_width, 
+                                      height=height_pixels, transform=self.ax.transAxes, facecolor=scale_color)
+                text_start -= shift
 
+            scale_text.set_x(text_start)
         self.ax.add_patch(scale_bar)
-        return self.ax
+                
+    def add_label(self, label_text, location='upper left', label_color='white',
+                 label_font_size=15):
+        """
+        Add a figure label to an image.
+
+        Parameters
+        ----------
+        label_text: str
+            image label
+        location: str or list
+            position of the label on the image, can be
+            'upper left', 'upper right', 'lower left', 'lower right' or
+            a list with xy coordinates [xpos, ypos] where 0 < xpos, ypos < 1
+        label_color: str
+            color of label
+        label_font_size: int
+            size of label
+
+        """
+        
+        r = self.ax.figure.canvas.get_renderer()
+        label_text = self.ax.text(x=0.05, y=0.05, s=label_text,
+                         transform=self.ax.transAxes, fontdict={'color':label_color, 'size':label_font_size})
+
+        # label seems far from top but accomodates e.g. accents/trema
+        image_height = self.ax.get_tightbbox(r).height
+        image_width = self.ax.get_tightbbox(r).width
+
+        text_height = label_text.get_window_extent(renderer=r).height / image_height
+        text_width = label_text.get_window_extent(renderer=r).width / image_width
+        
+        if isinstance(location, list):
+            if len(location) !=2:
+                raise Exception(f"You need to provide a pair of xy positions as a list [xpos, ypos].")
+            label_text.set_y(y=location[0])
+            label_text.set_x(x=location[1])
+                
+        if location == 'upper left':
+  
+            label_text.set_y(y=1-text_height)
+            label_text.set_x(x=0.01)
+        
+        elif location == 'upper right':
+  
+            label_text.set_y(y=1-text_height)
+            label_text.set_x(x=1-text_width-0.01)
+            
+        elif location == 'lower left':
+            
+            label_text.set_y(y=0.01)
+            label_text.set_x(x=0.01)
+            
+        elif location == 'lower right':
+  
+            label_text.set_y(y=0.01)
+            label_text.set_x(x=1-text_width-0.01)
