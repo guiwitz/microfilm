86d7d322eebdf24306840392eca998876c88aff1
guiwitz <guiwitz@users.noreply.github.com>
2021-05-05 16:29:13 +0200

    add export function, allow plain ndarray, fix ipywidget anim

--- microfilm/microanim.py
+++ microfilm/microanim.py
@@ -1,31 +1,81 @@
+from pathlib import Path
+
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
 import pandas as pd
 import ipywidgets as ipw
+import imageio
 from .microplot import microshow, multichannel_to_rgb
-
+from .dataset import Nparray
 class Microanim:
     
-    def __init__(self, data, channels, cmaps=None, flip_map=False, rescale_type='min_max',
+    def __init__(self, data, channels=None, cmaps=None, flip_map=False, rescale_type='min_max',
         limits=None, num_colors=256, scalebar=False, height_pixels=3, unit_per_pix=None, 
         scalebar_units=None, unit=None, scale_ypos=0.05, scale_color='white', scale_font_size=12,
-        scale_text_centered=False, ax=None):
+        scale_text_centered=False, ax=None, fig_scaling=3):
 
-        """Standard __init__ method.
+        """
+        Class implementing methods to create interactive animations via ipywidgets in notebooks
+        and to save animations as movies. Most options parameters are the same as for microplots.
         
         Parameters
         ----------
-        ax : matplotlib axis
+        data: microfilm.dataset.Data object or ndarray
+            object allowing for easy loading of images. If an
+            ndarray is used it needs dimension ordering CTXY
+        channels: list of str
+            list of channels from data object to plot
+        cmaps: list of str
+            colormap names
+        flip_map: bool or list of bool
+            invert colormap or not
+        rescale_type: str or list of str
+            'min_max': between extrema values of image
+            'dtype': full range of image dtype
+            'zero_max': between zero and image max
+            'limits': between limits given by parameter limits
+        limits: list or list of lists
+            [min, max] limits to use for rescaling
+        num_colors: int
+            number of steps in color scale
+        scalebar: bool
+            add scale bar or not
+        height_pixels: int
+            height of scale bar
+        unit_per_pix: float
+            pixel scaling (e.g. 25um per pixel)
+        scalebar_units: float
+            size of scale bar in true units
+        unit: str
+            name of the scale unit
+        scale_y_pos: float
+            y position of scale bar (0-1)
+        scale_color: str
+            color of scale bar
+        scale_font_size: int
+            size of text, set to None for no text
+        scale_text_centered: bool
+            center text above scale bar
+        ax: Matplotlib axis
+            provide existing axis
+        fig_scaling: int
+            control figure scaling
         
         Attributes
         ----------
         
         """
         
-        #self.ax = ax
+        if isinstance(data, np.ndarray):
+            data = Nparray(nparray=data)
+
         self.data = data
-        self.channels = channels
+        
+        if channels is None:
+            self.channels = self.data.channel_name
+        else:
+            self.channels = channels
 
         self.cmaps=cmaps
         self.flip_map=flip_map
@@ -36,7 +86,7 @@ class Microanim:
         self.time_slider = ipw.IntSlider(
             description="Time", min=0, max=self.data.K-1, value=0, continuous_update=True
         )
-        self.time_slider.observe(self.show_segmentation, names="value")
+        self.time_slider.observe(self.update_timeslider, names="value")
 
         self.output = ipw.Output()
         self.timestamps = None
@@ -49,16 +99,20 @@ class Microanim:
                 scalebar=scalebar, height_pixels=height_pixels, unit_per_pix=unit_per_pix,
                 scalebar_units=scalebar_units, unit=unit, scale_ypos=scale_ypos,
                 scale_color=scale_color, scale_font_size=scale_font_size,
-                scale_text_centered=scale_text_centered, ax=ax)
+                scale_text_centered=scale_text_centered, ax=ax, fig_scaling=fig_scaling)
 
         self.ui = ipw.VBox([self.output, self.time_slider])
             
 
-    def show_segmentation(self, change=None):
+    def update_timeslider(self, change=None):
         """Update segmentation plot"""
 
         t = self.time_slider.value
+        self.update_animation(t)
         
+    def update_animation(self, t):
+        """Update animation to time t"""
+
         images = [self.data.load_frame(x, t) for x in self.channels]
 
         converted = multichannel_to_rgb(images, cmaps=self.cmaps, flip_map=self.flip_map,
@@ -90,6 +144,45 @@ class Microanim:
         self.times = times.strftime('%H:%M:%S')
 
         self.timestamps = self.microim.add_label(self.times[0], location=location)
+
+    def save_movie(self, movie_name, fps=20, quality=5, format=None):
+        """Save a movie
+        
+        Parameters
+        ----------
+        movie_name: str or path object
+            where to save the movie
+        fps: int
+            frames per second
+        quality: int
+            quality of images (see imageio)
+        format: str
+            format for export
+
+        """
+
+        path_obj = Path(movie_name)
+
+        if path_obj.suffix in [".mov", ".avi", ".mpg", ".mpeg", ".mp4", ".mkv", ".wmv"]:
+            writer = imageio.get_writer(
+                path_obj,
+                fps=fps,
+                quality=quality,
+                format=format,
+            )
+        else:
+            writer = imageio.get_writer(path_obj, fps=fps, format=format)
+
+        for t in range(self.data.K-1):
+            self.update_animation(t)
+            self.microim.ax.figure.canvas.draw()
+            buf = np.frombuffer(self.microim.ax.figure.canvas.tostring_rgb(), dtype=np.uint8 )
+            w,h = self.microim.ax.figure.canvas.get_width_height()
+            buf.shape = (h, w, 3)
+            writer.append_data(buf)
+            
+        writer.close()
+
 
 
     
